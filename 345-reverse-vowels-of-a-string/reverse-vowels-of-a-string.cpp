f0dad71fda91ab0d563a1ef3f9f50b4bfc2d3299
Shubham Prajapati <prajapatishubhama2608@gmail.com>
2026-08-09 18:02:12 +0530

    Time: 7 ms (7.96%) | Memory: 10.3 MB (74.33%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    bool isVowel(char c) {
-        if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-            return true;
-        }
-        return false;
-    }
     string reverseVowels(string s) {
         int l = 0, r = s.size()-1;
 
         while(l <= r) {
-            if(isVowel(s[l]) && isVowel(s[r])) {
+            if((s[l] == 'A' || s[l] == 'E' || s[l] == 'I' || s[l] == 'O' || s[l] == 'U' || s[l] == 'a' || s[l] == 'e' || s[l] == 'i' || s[l] == 'o' || s[l] == 'u') && (s[r] == 'A' || s[r] == 'E' || s[r] == 'I' || s[r] == 'O' || s[r] == 'U' || s[r] == 'a' || s[r] == 'e' || s[r] == 'i' || s[r] == 'o' || s[r] == 'u')) {
                 swap(s[l], s[r]);
                 l++;
                 r--;
             }
-            else if(isVowel(s[l]) && (!isVowel(s[r]))) r--;
+            else if((s[l] == 'A' || s[l] == 'E' || s[l] == 'I' || s[l] == 'O' || s[l] == 'U' || s[l] == 'a' || s[l] == 'e' || s[l] == 'i' || s[l] == 'o' || s[l] == 'u') && !(s[r] == 'A' || s[r] == 'E' || s[r] == 'I' || s[r] == 'O' || s[r] == 'U' || s[r] == 'a' || s[r] == 'e' || s[r] == 'i' || s[r] == 'o' || s[r] == 'u')) r--;
             else l++;
         }
         return s;
