c84dfc3d562e418e2d1c5e63687e0da200e9285a
Shubham Prajapati <prajapatishubhama2608@gmail.com>
2026-09-19 10:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (75.89%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int dollor_5 = 0, dollor_10 = 0, dollor_20 = 0;
+
+        bool ans = true;
+
+        for(int i = 0; i < bills.size(); i++) {
+            if(bills[i] == 5) dollor_5++;
+
+            else if(bills[i] == 10) {
+                if(dollor_5 >= 1) {
+                    dollor_5--;
+                    dollor_10++;
+                } else {
+                    ans = false;
+                    break;
+                }
+            }
+            else {
+                if(dollor_5 >= 1 && dollor_10 >= 1) {
+                    dollor_5--;
+                    dollor_10--;
+                    dollor_20++;
+                } else if(dollor_5 >= 3) {
+                    dollor_20++;
+                    dollor_5 -= 3;
+                } else {
+                    ans = false;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
