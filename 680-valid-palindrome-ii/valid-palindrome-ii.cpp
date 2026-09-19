71da2b37174261b83c13787ee5b49a9ca7314adb
Shubham Prajapati <prajapatishubhama2608@gmail.com>
2026-09-19 15:16:38 +0530

    Time: 1 ms (79.00%) | Memory: 22.2 MB (56.61%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkPalindrome(string& str, int i, int j) {
+        while(i <= j) {
+            if(str[i] != str[j]) return false;
+            else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+
+        int i = 0, j = s.size()-1;
+
+        while(i < j) {
+            if(s[i] == s[j]) {
+                i++;
+                j--;
+            } else {
+                return checkPalindrome(s, i+1, j) || checkPalindrome(s, i, j-1);
+            }
+        }
+
+        return true;
+    }
+};
